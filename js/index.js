5c8e6e6958869422c3b788ebe0b3cbddb1bf2c9f
Takmim00 <takmimm@gmail.com>
2024-09-23 19:30:37 +0600

    donationTab and historyTab functionality added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+// donationTab and historyTab functionality
 const historyTab = document.getElementById("history-tab");
 const donationTab = document.getElementById("donation-tab");
 historyTab.addEventListener("click", function () {
@@ -13,5 +20,21 @@ historyTab.addEventListener("click", function () {
     "bg-[#B4F461]",
     "border-[#1111114D]"
   );
-  donationTab.classList.add("text-gray-500");
+  donationTab.classList.add("text-gray-500"); 
+  document.getElementById("donation-section").classList.add("hidden");
+  document.getElementById("history-list").classList.remove("hidden");
+});
+donationTab.addEventListener("click", function () {
+  donationTab.classList.add(
+    "text-black",
+    "bg-[#B4F461]",
+    "border-[#1111114D]"
+  );
+  historyTab.classList.remove(
+    "text-black",
+    "bg-[#B4F461]",
+    "border-[#1111114D]"
+  );
+  document.getElementById("donation-section").classList.remove("hidden");
+  document.getElementById("history-list").classList.add("hidden");
 });
